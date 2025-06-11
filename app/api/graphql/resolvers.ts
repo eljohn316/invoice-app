1fea540dd17eacde37fce1f0c1776655f3b8a2a2
El John <bongaeljohn07@gmail.com>
2025-06-11 14:39:32 +0800

    setup graphql server, implement get all invoices

---
+++ app/api/graphql/resolvers.ts
@@ -0,0 +1,20 @@
+import { db } from '@/lib/db';
+
+type Status = 'pending' | 'paid' | 'draft';
+
+export const resolvers = {
+  Query: {
+    invoices: async (_: any, args: { status?: Status }) => {
+      return await db.invoice.findMany({
+        where: {
+          status: args.status
+        },
+        include: {
+          clientAddress: true,
+          senderAddress: true,
+          items: true
+        }
+      });
+    }
+  }
+};
---
+++ app/api/graphql/schema.ts
@@ -0,0 +1,52 @@
+import { gql } from 'graphql-tag';
+
+export const typeDefs = gql`
+  enum Status {
+    pending
+    paid
+    draft
+  }
+
+  type SenderAddress {
+    id: Int!
+    street: String
+    city: String
+    postCode: String
+    country: String
+  }
+
+  type ClientAddress {
+    id: Int!
+    street: String
+    city: String
+    postCode: String
+    country: String
+  }
+
+  type Item {
+    id: Int!
+    name: String!
+    quantity: Int!
+    price: Float!
+    total: Float!
+  }
+
+  type Invoice {
+    id: ID!
+    createdAt: String!
+    paymentDue: String
+    description: String
+    paymentTerms: Int
+    clientName: String
+    clientEmail: String
+    status: Status
+    total: Float
+    senderAddress: SenderAddress
+    clientAddress: ClientAddress
+    items: [Item]!
+  }
+
+  type Query {
+    invoices(status: String): [Invoice]!
+  }
+`;
