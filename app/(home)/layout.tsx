7c4cea581967909f01ff7db0bb60fa3e20328475
El John <bongaeljohn07@gmail.com>
2025-06-14 16:59:44 +0800

    move Suspense component to page

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,10 +1,5 @@
-import { Suspense } from 'react';
 import { InvoicesProvider } from '@/app/(home)/_components/invoices-provider';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <InvoicesProvider>
-      <Suspense>{children}</Suspense>
-    </InvoicesProvider>
-  );
+  return <InvoicesProvider>{children}</InvoicesProvider>;
 }
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,16 @@
+'use client';
+
+import { Suspense } from 'react';
 import { InvoiceList } from '@/app/(home)/_components/invoice-list';
 import { InvoiceListHeader } from '@/app/(home)/_components/invoice-list-header';
 
 export default function Home() {
   return (
-    <div className="space-y-8 md:space-y-[3.4375rem] lg:space-y-16">
-      <InvoiceListHeader />
-      <InvoiceList />
-    </div>
+    <Suspense>
+      <div className="space-y-8 md:space-y-[3.4375rem] lg:space-y-16">
+        <InvoiceListHeader />
+        <InvoiceList />
+      </div>
+    </Suspense>
   );
 }
