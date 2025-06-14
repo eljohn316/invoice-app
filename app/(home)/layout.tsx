f0d7856ebcea5666078c542dde91bfb7cc21e7b5
El John <bongaeljohn07@gmail.com>
2025-06-14 17:12:32 +0800

    fix deployment error

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,5 +1,9 @@
-import { InvoicesProvider } from '@/app/(home)/_components/invoices-provider';
+import { Suspense } from 'react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <InvoicesProvider>{children}</InvoicesProvider>;
+  return (
+    <Suspense>
+      <div className="space-y-8 md:space-y-[3.4375rem] lg:space-y-16">{children}</div>
+    </Suspense>
+  );
 }
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,16 +1,12 @@
-'use client';
-
-import { Suspense } from 'react';
 import { InvoiceList } from '@/app/(home)/_components/invoice-list';
 import { InvoiceListHeader } from '@/app/(home)/_components/invoice-list-header';
+import { InvoicesProvider } from '@/app/(home)/_components/invoices-provider';
 
 export default function Home() {
   return (
-    <Suspense>
-      <div className="space-y-8 md:space-y-[3.4375rem] lg:space-y-16">
-        <InvoiceListHeader />
-        <InvoiceList />
-      </div>
-    </Suspense>
+    <InvoicesProvider>
+      <InvoiceListHeader />
+      <InvoiceList />
+    </InvoicesProvider>
   );
 }
