2286fb0d5dc04edaa47c8bb353b98e60fac08feb
El John <bongaeljohn07@gmail.com>
2025-06-21 16:05:47 +0800

    moved Suspense component to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { ThemeProvider } from '@/components/theme-provider';
 import { SWRProvider } from '@/components/swr-provider';
 import { cn } from '@/lib/utils';
 import { InvoiceListProvider } from '@/components/invoice-list-provider';
+import { Suspense } from 'react';
 
 const leagueSpartan = League_Spartan({
   variable: '--font-league-spartan',
@@ -30,20 +31,22 @@ export default function RootLayout({
           'flex min-h-svh flex-col overflow-y-scroll bg-[#F8F8FB] antialiased lg:flex-row dark:bg-[#141625]',
           leagueSpartan.variable
         )}>
-        <SWRProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange>
-            <InvoiceListProvider>
-              <Navigation />
-              <div className="relative flex-1 px-6 py-8 md:px-12 md:py-[3.75rem] lg:mx-auto lg:max-w-[45.625rem] lg:px-4 lg:py-[4.875rem]">
-                {children}
-              </div>
-            </InvoiceListProvider>
-          </ThemeProvider>
-        </SWRProvider>
+        <Suspense>
+          <SWRProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange>
+              <InvoiceListProvider>
+                <Navigation />
+                <div className="relative flex-1 px-6 py-8 md:px-12 md:py-[3.75rem] lg:mx-auto lg:max-w-[45.625rem] lg:px-4 lg:py-[4.875rem]">
+                  {children}
+                </div>
+              </InvoiceListProvider>
+            </ThemeProvider>
+          </SWRProvider>
+        </Suspense>
       </body>
     </html>
   );
