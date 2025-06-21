637f150ac8aca99c7227d0b2f3c68e112daded79
El John <bongaeljohn07@gmail.com>
2025-06-21 16:06:56 +0800

    remove unused Suspense import

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,5 +1,3 @@
-import { Suspense } from 'react';
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <div className="space-y-8 md:space-y-[3.4375rem] lg:space-y-16">{children}</div>;
 }
